63c7161e62a0351909a1bbe22fceba0b78e8c319
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2012-02-20 23:52:01 +0100

    fixed water lily

--- src/de/xghostkillerx/blocksonglass/blocks/CustomWaterLily.java
+++ src/de/xghostkillerx/blocksonglass/blocks/CustomWaterLily.java
@@ -2,7 +2,9 @@ package de.xghostkillerx.blocksonglass.blocks;
 
 import net.minecraft.server.Block;
 import net.minecraft.server.BlockWaterLily;
+import net.minecraft.server.Material;
 import net.minecraft.server.StepSound;
+import net.minecraft.server.World;
 
 public class CustomWaterLily extends BlockWaterLily {
 
@@ -32,5 +34,12 @@ public class CustomWaterLily extends BlockWaterLily {
 				i == Block.WOOD_STAIRS.id || i == Block.NETHER_BRICK_STAIRS.id || i == Block.BRICK_STAIRS.id ||
 				i == Block.COBBLESTONE_STAIRS.id || i == Block.PISTON.id || i == Block.PISTON_STICKY.id ||
 				i == Block.PISTON_EXTENSION.id || i == Block.PISTON_MOVING.id;
+    }
+    
+    public boolean f(World world, int i, int j, int k) {
+    	if (j >= 0 && j < world.height) {
+    		return world.getData(i, j - 1, k) == 0;
+    	}
+    	else return false;
     }
 }
